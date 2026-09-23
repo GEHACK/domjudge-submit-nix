0d0b0db3894b1d62af8fa233789543a435f56ab3
Bartjan Henkemans <bartjan@henkemans.be>
2026-09-23 23:05:35 +0200

    fix: automation

--- flake.nix
+++ flake.nix
@@ -22,7 +22,6 @@
       devShells.x86_64-linux.default = pkgs.mkShell {
         packages = with pkgs; [
           jq
-          nix-prefetch-github
         ];
       };
     };
--- update.sh
+++ update.sh
@@ -12,7 +12,7 @@ repo=$(jq -r .repo source.json)
 
 rev=$(git ls-remote "https://github.com/$owner/$repo" "$1" "refs/tags/$1^{}" "refs/heads/$1" | cut -f1 | tail -n1)
 rev=${rev:-$1}
-hash=$(nix-prefetch-github "$owner" "$repo" --rev "$rev" | jq -r .hash)
+hash=$(nix flake prefetch --json "github:$owner/$repo/$rev" | jq -r .hash)
 
 jq --arg rev "$rev" --arg hash "$hash" '.rev = $rev | .hash = $hash' source.json > source.json.tmp
 mv source.json.tmp source.json
